3593bb550f46c1f46472942ccec9243a5d908ad5
Eder Dureaux <1mmolationstayhc@gmail.com>
2024-10-14 00:15:06 -0300

    Back button, theme color, etc

--- src/app/personal/personal.page.ts
+++ src/app/personal/personal.page.ts
@@ -1,11 +1,27 @@
 import { Component, OnInit } from '@angular/core';
 
+interface Personal {
+  icon: string;
+  name: string;
+  redirecTo: string;
+}
+
 @Component({
   selector: 'app-personal',
   templateUrl: './personal.page.html',
   styleUrls: ['./personal.page.scss'],
 })
 export class PersonalPage implements OnInit {
+
+  personales: Personal[] = [  
+    {
+      icon: "person-circle-outline",
+      name: "perfil",
+      redirecTo: "/perfil"
+    },
+  ];
+
+
 
   constructor() { }
 
